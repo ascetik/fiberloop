38750f7cf411c17d023a19196322c468458ce327
ascetik <ascetik@free.fr>
2023-10-19 19:16:35 +0200

    import of TaskReportRegistry
    still with missing sources

---
+++ src/Containers/TaskReportRegistry.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This is part of the FiberLoop package.
+ *
+ * @package    FiberLoop
+ * @category   Container
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
+declare(strict_types=1);
+
+namespace Ascetik\Fiberloop\Containers;
+
+use Ascetik\FiberLoop\DTO\TaskReport;
+use Ascetik\Storage\Box;
+
+/**
+ * Register task reports
+ * @version 1.0.0
+ */
+final class TaskReportRegistry extends Box
+{
+    public function push(TaskReport $report)
+    {
+        $this->attach($report);
+    }
+}
